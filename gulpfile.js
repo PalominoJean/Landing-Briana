37f40f8b42d8b86a9eb0c729814c6b30d6c77f8a
bryan <acuariobr@gmail.com>
2016-06-22 17:44:30 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var del = require('del');
+var browsersync = require('browser-sync').create();
+var reload = browsersync.reload;
+var paths = {
+    styles: [
+        './styles/**/*.scss',
+    ],
+    image: [
+        './images/**/*.{png,jpg,jpeg}'
+    ],
+    fonts: [
+        './fonts/**/*.{ttf,eot,woff,woff2}',
+        './bower_components/font-awesome/fonts/*.{ttf,eot,woff,woff2}'
+    ],
+    script: [
+        './scripts/*.js',
+        './node_modules/jquery/dist/jquery.min.js'
+    ],
+    templates: [
+        './*.html',
+    ]
+}
+
+gulp.task('html', function() {
+    return gulp
+        .src(paths.templates)
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('styles', function() {
+    return gulp
+        .src(paths.styles)
+        .pipe(sass()).on('error', handleError)
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('images', function() {
+    return gulp
+        .src(paths.image)
+        .pipe(gulp.dest('dist/images'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('scripts', function() {
+    return gulp
+        .src(paths.script)
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+gulp.task('fonts', function() {
+    return gulp
+        .src(paths.fonts)
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('clean', function(cb) {
+    return del('dist', cb);
+});
+
+gulp.task('server', function() {
+    browsersync.init({
+        server: {
+            baseDir: './dist'
+        },
+        browser: 'google-chrome'
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.templates, ['html']);
+    gulp.watch(paths.styles, ['styles']);
+    gulp.watch(paths.image, ['images']);
+    gulp.watch(paths.fonts, ['fonts']);
+    gulp.watch(paths.script, ['scripts']);
+});
+
+gulp.task('build', ['styles', 'images', 'fonts', 'html', 'scripts']);
+gulp.task('default', ['build', 'server', 'watch']);
+
+function handleError(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
